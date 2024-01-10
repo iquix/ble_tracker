bbd48afe06a2b4c08cb3741544754a2f6a340688
iquix <49198015+iquix@users.noreply.github.com>
2024-01-10 17:15:57 +0900

    Fix constants for changes in HA 2024.1
    Fix constants for changes in HA 2024.1

--- custom_components/ble_tracker/device_tracker.py
+++ custom_components/ble_tracker/device_tracker.py
@@ -7,11 +7,13 @@ import voluptuous as vol
 from aioblescan.plugins import EddyStone
 from typing import Optional, Set, Tuple
 
-from homeassistant.components.device_tracker import PLATFORM_SCHEMA
+from homeassistant.components.device_tracker import (
+    PLATFORM_SCHEMA,
+    SourceType,
+)
 from homeassistant.components.device_tracker.const import (
     CONF_TRACK_NEW,
     DEFAULT_TRACK_NEW,
-    SOURCE_TYPE_BLUETOOTH_LE,
 )
 from homeassistant.components.device_tracker.legacy import (
     YAML_DEVICES,
@@ -57,7 +59,7 @@ async def see_device(hass, async_see, mac, device_name, rssi=None):
         mac=BLE_PREFIX+mac,
         host_name=device_name,
         attributes=attributes,
-        source_type=SOURCE_TYPE_BLUETOOTH_LE,
+        source_type=SourceType.BLUETOOTH_LE,
     )
 
 
